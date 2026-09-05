4b7a618ea330be1e446171b15f76ac51a83a35d5
Marcus Mason <marcusm41705@vt.edu>
2026-09-04 22:37:20 -0400

    Additional adds + implementation

--- unit_test.cpp
+++ unit_test.cpp
@@ -18,3 +18,55 @@ TEST_CASE("treap unit test1", "[treap]") {
 	REQUIRE(res->key == 50);
 
 }
+TEST_CASE("Inserting Node", "[treap]"){
+	TreapNode *t = NULL;
+	t = insert(t, 75);
+	TreapNode *t_r = search(t, 75);
+	REQUIRE(t_r != NULL);
+	REQUIRE(t_r->key == 75);
+}
+TEST_CASE("Searching for Node/Missing", "[treap]"){
+	TreapNode *t = NULL;
+	t = insert(t, 40);
+	t = insert(t, 80);
+	t = insert(t, 90);
+	TreapNode *t_s = search(t,100);
+	REQUIRE(t_s == NULL);
+}
+TEST_CASE("Insert Multiple Nodes/Search Testing", "[treap]"){
+	TreapNode *t = NULL;
+	t = insert(t,30);
+	t = insert(t,40);
+	t = insert(t,50);
+	t = insert(t,60);
+	t = insert(t,70);
+	REQUIRE(search(t,30) != NULL);
+	REQUIRE(search(t,40) != NULL);
+	REQUIRE(search(t,50) != NULL);
+	REQUIRE(search(t,60) != NULL);
+	REQUIRE(search(t,70) != NULL);
+	
+}
+TEST_CASE("Deleting EXISTING Node", "[treap]"){
+	TreapNode *t = NULL;
+	t = insert(t,10);
+	t = insert(t,20);
+	t = insert(t,30);
+	t = insert(t,40);
+	t = deleteNode(t,30);
+	REQUIRE(search(t,30) == NULL);
+	REQUIRE(search(t,10) != NULL);
+
+}
+TEST_CASE("Deleting Non Existent Node Test", "[treap]"){
+	TreapNode *t = NULL;
+	t = insert(t,10);
+	t = insert(t,20);
+	t = insert(t,30);
+	t = deleteNode(t,50);
+	REQUIRE(search(t,10) != NULL);
+	REQUIRE(search(t,20) != NULL);
+	REQUIRE(search(t,30) != NULL);
+
+}
+
