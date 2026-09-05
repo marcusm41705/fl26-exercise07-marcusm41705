b54c94a90a61742634465eaf45179ea2881c554c
Marcus Mason <marcusm41705@vt.edu>
2026-09-04 22:19:09 -0400

    Added files and CMakeLists

---
+++ build/CMakeFiles/clientcode.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/marcu/ECE_Software_Des/applied_software_coursework/fl26-exercise07-marcusm41705/clientcode.cpp" "CMakeFiles/clientcode.dir/clientcode.cpp.obj" "gcc" "CMakeFiles/clientcode.dir/clientcode.cpp.obj.d"
+  "C:/Users/marcu/ECE_Software_Des/applied_software_coursework/fl26-exercise07-marcusm41705/treap.cpp" "CMakeFiles/clientcode.dir/treap.cpp.obj" "gcc" "CMakeFiles/clientcode.dir/treap.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/clientcode.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clientcode.dir/clientcode.cpp.obj"
+  "CMakeFiles/clientcode.dir/clientcode.cpp.obj.d"
+  "CMakeFiles/clientcode.dir/treap.cpp.obj"
+  "CMakeFiles/clientcode.dir/treap.cpp.obj.d"
+  "clientcode.exe"
+  "clientcode.exe.manifest"
+  "clientcode.pdb"
+  "libclientcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clientcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
