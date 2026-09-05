87d94a18a8308c9d8dbda8d0a2d9196866128f5f
Marcus Mason <marcusm41705@vt.edu>
2026-09-04 22:39:40 -0400

    Additional debugging

--- unit_test.cpp
+++ unit_test.cpp
@@ -11,7 +11,7 @@
 
 TEST_CASE("treap unit test1", "[treap]") {
 
-	struct TreapNode *root = NULL; 
+	struct TreapNode *root = nullptr; 
 	root = insert(root, 50);
 	
 	TreapNode *res = search(root, 50);
@@ -19,54 +19,54 @@ TEST_CASE("treap unit test1", "[treap]") {
 
 }
 TEST_CASE("Inserting Node", "[treap]"){
-	TreapNode *t = NULL;
+	TreapNode *t = nullptr;
 	t = insert(t, 75);
 	TreapNode *t_r = search(t, 75);
-	REQUIRE(t_r != NULL);
+	REQUIRE(t_r != nullptr);
 	REQUIRE(t_r->key == 75);
 }
 TEST_CASE("Searching for Node/Missing", "[treap]"){
-	TreapNode *t = NULL;
+	TreapNode *t = nullptr;
 	t = insert(t, 40);
 	t = insert(t, 80);
 	t = insert(t, 90);
 	TreapNode *t_s = search(t,100);
-	REQUIRE(t_s == NULL);
+	REQUIRE(t_s == nullptr);
 }
 TEST_CASE("Insert Multiple Nodes/Search Testing", "[treap]"){
-	TreapNode *t = NULL;
+	TreapNode *t = nullptr;
 	t = insert(t,30);
 	t = insert(t,40);
 	t = insert(t,50);
 	t = insert(t,60);
 	t = insert(t,70);
-	REQUIRE(search(t,30) != NULL);
-	REQUIRE(search(t,40) != NULL);
-	REQUIRE(search(t,50) != NULL);
-	REQUIRE(search(t,60) != NULL);
-	REQUIRE(search(t,70) != NULL);
+	REQUIRE(search(t,30) != nullptr);
+	REQUIRE(search(t,40) != nullptr);
+	REQUIRE(search(t,50) != nullptr);
+	REQUIRE(search(t,60) != nullptr);
+	REQUIRE(search(t,70) != nullptr);
 	
 }
 TEST_CASE("Deleting EXISTING Node", "[treap]"){
-	TreapNode *t = NULL;
+	TreapNode *t = nullptr;
 	t = insert(t,10);
 	t = insert(t,20);
 	t = insert(t,30);
 	t = insert(t,40);
 	t = deleteNode(t,30);
-	REQUIRE(search(t,30) == NULL);
-	REQUIRE(search(t,10) != NULL);
+	REQUIRE(search(t,30) == nullptr);
+	REQUIRE(search(t,10) != nullptr);
 
 }
 TEST_CASE("Deleting Non Existent Node Test", "[treap]"){
-	TreapNode *t = NULL;
+	TreapNode *t = nullptr;
 	t = insert(t,10);
 	t = insert(t,20);
 	t = insert(t,30);
 	t = deleteNode(t,50);
-	REQUIRE(search(t,10) != NULL);
-	REQUIRE(search(t,20) != NULL);
-	REQUIRE(search(t,30) != NULL);
+	REQUIRE(search(t,10) != nullptr);
+	REQUIRE(search(t,20) != nullptr);
+	REQUIRE(search(t,30) != nullptr);
 
 }
 
